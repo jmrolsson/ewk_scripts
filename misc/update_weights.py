c00a1c0dd1cf805075ba47cead9da5a458fa21e6
Mats Joakim Robert Olsson <joakim.olsson@cern.ch>
2017-09-18 20:34:49 -0500

    fixed update_weights.py script

--- misc/update_weights.py
+++ misc/update_weights.py
@@ -19,6 +19,7 @@ import argparse
 import json
 import re
 import io
+import sys
 
 try:
   import ROOT
@@ -43,7 +44,7 @@ if __name__ == '__main__':
     parser.add_argument('file_paths', metavar='<file_paths>', type=str, help='Input file pattern. Example: \'./my_path/*.root\'')
     parser.add_argument('--cutflow-bin', dest='cutflow_bin', type=int, required=False, help='Cutflow bin to read', default=2)
     parser.add_argument('--weights', metavar='<weights.json>', type=str, help='JSON dictionary of weights', default = 'weights.json')
-    parser.add_argument('--threshold', type=float, help='Threshold for number comparison', default = 0.00001)
+    parser.add_argument('--threshold', type=float, help='Threshold for number comparison', default = 0.001)
     parser.add_argument('-o', '--output', dest='output_file', metavar='<weights_modified.json>', type=str, help='Updated JSON dictionary of weights.', default = 'weights_modified.json')
     parser.add_argument('-v', '--verbose', dest='verbose', action='count', default=0, help='Enable verbose output of various levels. Default: no verbosity')
     args = parser.parse_args()
@@ -59,23 +60,25 @@ if __name__ == '__main__':
         weights = json.load(file(args.weights))
 
         file_paths = glob.glob(args.file_paths)
+        logger.info('Begin processing files...')
         for f in file_paths:
-            logger.debug('\n----> Input file: '+f)
+            logger.debug('Input file:\n '+f)
             did = get_did(f)
             if did in weights:
-              logger.info('DID {} found in {}'.format(did, args.weights))
+              logger.debug('DID {} found in {}'.format(did, args.weights))
               logger.debug('Dictionary entry for DID {}: {}'.format(did, weights[did]))
             else:
-              logger.error('DID {} not found in weight'.format(did, args.weights))
-              sys.exit(1)
+              logger.warning('DID {} not found in {}'.format(did, args.weights))
+              continue
 
             root_file = TFile.Open(f, 'READ')
             nevt_dict = weights[did]['num events']
             nevt_actual = root_file.Get('cut_flow').GetBinContent(2)
             logger.debug('num events in weights dictionary: {}'.format(nevt_dict))
             logger.debug('actual num events in dataset root file: {}'.format(nevt_actual))
-            if (float(nevt_actual)/float(nevt_dict) < args.threshold):
+            if (abs(float(nevt_actual)/float(nevt_dict) - 1.000) > args.threshold):
                 logger.debug('--> Mismatch between weights dictionary and actual number of events, updating dictionary for DID: {}'.format(did))
+                logger.info('({}) \'num events\': {} -> {}, ratio: {}'.format(did, nevt_dict, nevt_actual, nevt_actual/nevt_dict))
                 weights[did]['num events'] = nevt_actual
 
         # write JSON
@@ -86,6 +89,8 @@ if __name__ == '__main__':
         with io.open(args.output_file, 'w', encoding='utf8') as f_json:
             f_json.write(to_unicode(json.dumps(weights, indent=2, sort_keys=True,  separators=(',', ': '), ensure_ascii=False)))
             logger.info('Saved updated weights dictionary to: {}'.format(args.output_file))
+
+        logger.info('Done!')
 
     except IOError:
         logger.error('No weights file found named \'{0:s}\' (specify by \'--weights <weights.json>\')'.format(args.weights))
